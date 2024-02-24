029480a1b2207a5842537c03cff7b0a0cf3b892b
Abdullah-Shoman <abdulla.a96@outlook.com>
2024-02-24 18:22:04 +0200

    modified after code review

--- python_stack/_python/OOP/assignment_banck_account/bank_account.py
+++ python_stack/_python/OOP/assignment_banck_account/bank_account.py
@@ -1,10 +1,18 @@
 
 
 class bank_account:
+    # BankAccount 
 
-    def __init__(self, amount =0,int_rate=0.01):
+    def __init__(self, amount =0,int_rate=1.2):
         self.balance = amount
         self.int_rate = int_rate;
+    
+    # def __init__(self):
+    #     pass 
+
+    @classmethod 
+    def test(cls,amount):
+        return cls(amount,0)
 
     def deposit(self,amount):
         self.balance += amount
@@ -27,9 +35,12 @@ class bank_account:
             self.balance = self.balance+  (self.int_rate * self.balance)
         return self
 
-account1 = bank_account()
-account2 = bank_account()
+account1 = bank_account(12,1)
+account2 = bank_account().test(11)
 
-account1.deposit(100).deposit(200).deposit(500).withdraw(100).yield_interest().display_account_info()
+account1.display_account_info()
+account2.display_account_info()
 
-account2.deposit(200).deposit(100).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()
+# account1.deposit(100).deposit(200).deposit(500).withdraw(100).yield_interest().display_account_info()
+
+# account2.deposit(200).deposit(100).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()

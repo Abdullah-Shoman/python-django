520aa754e62b318393ea11495e88c871f278577d
Abdullah-Shoman <abdulla.a96@outlook.com>
2024-02-29 00:06:36 +0200

    update assignemnt checkerboarder

--- python_stack/flask/flask_fundamental/assign_checkerboard/main.py
+++ python_stack/flask/flask_fundamental/assign_checkerboard/main.py
@@ -1,24 +1,42 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
-def map1():
-    row = 8
-    col = 8
-    return render_template('index.html',col = col ,row = row)
+def main():
+    num = 8
+    num2 = 8
+    color1 = 'gold'
+    color2 = 'goldenrod'
+    return render_template('index.html',num=num,num2=num2,color1=color1,color2=color2)
+
 
 @app.route('/<num>')
-def map2(num):
-    row = 8
-    col = int(num)
-    return render_template('index.html',col = col ,row = row)
+def main2(num):
+    num2 = 8
+    num = int(num)
+    color1 = 'gold'
+    color2 = 'goldenrod'
+    return render_template('index.html',num=num,num2=num2,color1=color1,color2=color2)
 
 @app.route('/<num>/<num2>')
-def map3(num,num2):
-    row = int(num2)
-    col = int(num)
-    return render_template('index.html',col = col ,row = row)
+def main3(num,num2):
+    num = int(num)
+    num2 = int(num2)
+    color1 = 'gold'
+    color2 = 'goldenrod'
+    return render_template('index.html',num=num,num2=num2,color1=color1,color2=color2)
+
+
+
+@app.route('/<num>/<num2>/<color1>/<color2>')
+def main4(num,num2,color1,color2):
+    color1 = color1
+    color2 = color2
+    num = int(num)
+    num2 = int(num2)
+    return render_template('index.html',color1=color1,color2=color2,num=num,num2=num2)
 
 
 if __name__ == '__main__':
